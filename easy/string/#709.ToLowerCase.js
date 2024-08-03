dc7576b78430169ad505e2796d6263933c5e77b1
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-08-03 22:24:10 +0500

    #709 Runtime 53ms Beats 52.67% Memory 48.72 MB Beats 66.20%

---
+++ easy/string/#709.ToLowerCase.js
@@ -0,0 +1,30 @@
+/**Given a string s, return the string after replacing every uppercase letter with the same lowercase letter. */
+
+/**Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely" */
+
+/** */
+
+/**Runtime 53 ms
+Beats 52.67%
+ */
+
+/**Memory 48.72 MB
+Beats 66.20%
+ */
+
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+toLowerCase("Hello");
