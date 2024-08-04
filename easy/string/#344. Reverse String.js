7f2c92d02314c48832cbbf77d0ca1bb3e5614eb6
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-08-04 23:06:00 +0500

    #344 Runtime 78ms Beats 60.07%
    Memory 55.37MB Beats 96.79%

---
+++ easy/string/#344. Reverse String.js
@@ -0,0 +1,25 @@
+/**Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory. */
+
+/**Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H" */
+
+/**Runtime 78 ms
+Beats 60.07%
+
+Memory 55.37 MB
+Beats 96.79%
+ */
+
+var reverseString = function (s) {
+  return s.reverse();
+};
+
+reverseString();
