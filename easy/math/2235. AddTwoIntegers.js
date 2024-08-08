3600715e244162c7f2429f0cc35d92daf0778d8f
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-08-09 00:01:16 +0500

    #2235 Runtime 52 ms Beats 59.86%
    Memory 48.56 MB
    Beats 82.41%

---
+++ easy/math/2235. AddTwoIntegers.js
@@ -0,0 +1,20 @@
+/**Given two integers num1 and num2, return the sum of the two integers. */
+
+/**Runtime
+52
+ms
+Beats
+59.86%
+Analyze Complexity
+Memory
+48.56
+MB
+Beats
+82.41%
+ */
+
+var sum = function (num1, num2) {
+  return num1 + num2;
+};
+
+sum(12, 12);
