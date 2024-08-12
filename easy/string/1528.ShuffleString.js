897ea756c0a1a92603758afc1e044ac8206c8dc6
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-08-12 23:08:59 +0500

    initial commit

--- easy/string/1528.ShuffleString.js
+++ easy/string/1528.ShuffleString.js
@@ -13,7 +13,7 @@ Explanation: After shuffling, each character remains in its position. */
 Beats 80.20%
 
 Memory 51.08 MB
-Beats20.71% */
+Beats 20.71% */
 
 var restoreString = function (s, indices) {
   let ar = new Array(indices.length);
